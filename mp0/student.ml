8a78bd1641a00cd0cdea2e6818d759eefd2765c0
Chirag Patel <chiragpat@gmail.com>
2013-01-15 14:09:08 -0600

    initial

---
+++ mp0/student.ml
@@ -0,0 +1,34 @@
+(* CS421 - Spring 2011
+ * MP0
+ *
+ * Please keep in mind that there may be more than one
+ * way to solve a problem.  You will want to change how a number of these start.
+ *)
+
+open Mp0common
+
+(* Problem 1 *)
+let a = 0;;  (* You will want to change this. *)
+
+(* Problem 2 *)
+let s = "Something else you will want to change";;
+
+(* Problem 3 *)
+let add_a n =
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 4 *)
+let s_paired_with_a_times b =
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 5 *)
+let greetings name =
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 6 *)
+let greetstring name =
+  raise(Failure "Function not implemented yet.")
+
+(*Problem 7 *)
+let sign n =
+  raise(Failure "Function not implemented yet.")
